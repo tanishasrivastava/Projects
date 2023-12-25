a7273098b2ee1cc4a5fe3fd41a0c20037ef02cc6
Tanisha Srivastava <138584422+tanishasrivastava@users.noreply.github.com>
2023-12-25 13:50:50 +0530

    HostelManagement_Project.c
    Hostel Management System in C
    
    This project is an implementation of a simple Hostel Management System using the C programming language. It allows users to manage and manipulate student records within a hostel environment.
    
    Key Features:
    
    Add New Entries: Add new student records including details like name, age, room number, and phone number.
    View Student Records: Display the entire list of student records stored in the system.
    Edit Student Information: Modify details of existing student records by specifying the roll number.
    File Handling: The system utilizes file handling to store and retrieve student information.
    How to Use:
    
    Adding New Entries: Input student details to add new entries to the system.
    Viewing Student Records: Retrieve and view the complete list of student records.
    Editing Student Information: Modify specific details of a student by providing their roll number.

--- HostelManagement_Project.c
+++ HostelManagement_Project.c
@@ -2,7 +2,6 @@
 #include <stdlib.h>
 #include <string.h>
 
-// Define the structure for student details
 struct Student {
     char name[60];
     int age;
@@ -36,15 +35,25 @@ void view_stud() {
 void new_entry() {
     FILE *fp;
     Student add;
-    Student check;
     
-    fp = fopen("hostel.txt", "a+");
+    fp = fopen("hostel.txt", "a");
     if (fp == NULL) {
         printf("File can't be opened.\n");
         return;
     }
 
-    // Your implementation for adding new student entries
+    printf("Enter student name: ");
+    scanf("%s", add.name);
+    printf("Enter student age: ");
+    scanf("%d", &add.age);
+    printf("Enter room number: ");
+    scanf("%s", add.room_no);
+    printf("Enter phone number: ");
+    scanf("%d", &add.phone);
+    printf("Enter roll number: ");
+    scanf("%s", add.roll_no);
+
+    fprintf(fp, "%s %d %s %d %s\n", add.name, add.age, add.room_no, add.phone, add.roll_no);
 
     fclose(fp);
     printf("\nStudent added successfully!\n");
@@ -52,8 +61,7 @@ void new_entry() {
 
 void edit() {
     FILE *old, *newrec;
-    Student check;
-    Student upd;
+    Student check, upd;
     int choice, test = 0;
 
     old = fopen("hostel.txt", "r");
@@ -64,11 +72,53 @@ void edit() {
         return;
     }
 
-    // Your implementation for editing student information
+    printf("Enter the roll no. of the student whose info you want to change: ");
+    scanf("%s", upd.roll_no);
 
+    while (fscanf(old, "%s %d %s %d %s\n", check.name, &check.age, check.room_no, &check.phone, check.roll_no) != EOF) {
+        if (strcmp(check.roll_no, upd.roll_no) == 0) {
+            test = 1;
+            printf("Which information do you want to change?\n");
+            printf("1. Name\n2. Age\n3. Room No\n4. Phone No\n5. All of the above\nEnter your choice: ");
+            scanf("%d", &choice);
+
+            if (choice == 1) {
+                printf("Enter new name: ");
+                scanf("%s", upd.name);
+                fprintf(newrec, "%s %d %s %d %s\n", upd.name, check.age, check.room_no, check.phone, check.roll_no);
+            } else if (choice == 2) {
+                printf("Enter new age: ");
+                scanf("%d", &upd.age);
+                fprintf(newrec, "%s %d %s %d %s\n", check.name, upd.age, check.room_no, check.phone, check.roll_no);
+            } else if (choice == 3) {
+                printf("Enter new room number: ");
+                scanf("%s", upd.room_no);
+                fprintf(newrec, "%s %d %s %d %s\n", check.name, check.age, upd.room_no, check.phone, check.roll_no);
+            } else if (choice == 4) {
+                printf("Enter new phone number: ");
+                scanf("%d", &upd.phone);
+                fprintf(newrec, "%s %d %s %d %s\n", check.name, check.age, check.room_no, upd.phone, check.roll_no);
+            } else if (choice == 5) {
+                printf("Enter new name: ");
+                scanf("%s", upd.name);
+                printf("Enter new age: ");
+                scanf("%d", &upd.age);
+                printf("Enter new room number: ");
+                scanf("%s", upd.room_no);
+                printf("Enter new phone number: ");
+                scanf("%d", &upd.phone);
+                fprintf(newrec, "%s %d %s %d %s\n", upd.name, upd.age, upd.room_no, upd.phone, check.roll_no);
+            }
+            printf("Changes saved!\n");
+        } else {
+            fprintf(newrec, "%s %d %s %d %s\n", check.name, check.age, check.room_no, check.phone, check.roll_no);
+        }
+    }
     fclose(old);
     fclose(newrec);
-    printf("\nChanges saved!\n");
+
+    remove("hostel.txt");
+    rename("new.txt", "hostel.txt");
 }
 
 int main() {
